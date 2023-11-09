9bc0df3c1516c26d1080c6875f2050edaa880c5d
agonzolm@myuax.com <agonzolm@myuax.com>
2023-11-09 13:17:01 +0100

    10
    creamos una funcion con una lista de estudiantes de diferentes grados

---
+++ filtrado.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Estudiante {
+public:
+    std::string nombre;
+    int edad;
+    std::string grado;
+
+    Estudiante(const std::string& nombre, int edad, const std::string& grado) {
+        this->nombre = nombre;
+        this->edad = edad;
+        this->grado = grado;
+    }
+};
+
+std::vector<Estudiante> filtrarEstudiantesPorGrado(const std::vector<Estudiante>& estudiantes, const std::string& grado) {
+    std::vector<Estudiante> estudiantesFiltrados;
+
+    for (const Estudiante& estudiante : estudiantes) {
+        if (estudiante.grado == grado) {
+            estudiantesFiltrados.push_back(estudiante);
+        }
+    }
+
+    return estudiantesFiltrados;
+}
+
+int main() {
+
+    std::vector<Estudiante> estudiantes;
+    estudiantes.push_back(Estudiante("Jorge", 18, "Ingenieria Informatica"));
+    estudiantes.push_back(Estudiante("María", 17, "Derecho"));
+    estudiantes.push_back(Estudiante("Alvaro", 18, "Marketing"));
+    estudiantes.push_back(Estudiante("Rosa", 16, "ADE"));
+
+
