ef82f41619a344dda5e175bc24d2a509a64f81da
agonzolm@myuax.com <agonzolm@myuax.com>
2023-11-09 12:14:01 +0100

    2

--- operacionesbasicas.cpp
+++ operacionesbasicas.cpp
@@ -5,4 +5,20 @@ int sumaDosNumeros(int num1, int num2) {
     return resultado;
 }
 
+int main() {
+    int numero1, numero2;
 
+
+    std::cout << "Ingrese el primer número: ";
+    std::cin >> numero1;
+
+    std::cout << "Ingrese el segundo número: ";
+    std::cin >> numero2;
+
+ 
+    int resultado = sumaDosNumeros(numero1, numero2);
+
+    std::cout << "La suma de " << numero1 << " y " << numero2 << " es: " << resultado << std::endl;
+
+    return 0;
+}
